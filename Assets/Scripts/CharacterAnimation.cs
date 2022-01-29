de46b7a8dec093e1fc67f2ea5941e4a6bf50693b
Tarmo Jussila <tarmo.jussila@zaibatsu.fi>
2022-01-29 17:09:02 +0200

    Deceleration for movement anims.

--- Assets/Scripts/CharacterAnimation.cs
+++ Assets/Scripts/CharacterAnimation.cs
@@ -15,23 +15,41 @@ public class CharacterAnimation : MonoBehaviour
     private readonly float armMovementAngle = 60f;
     private readonly float armRotationOffset = 70f;
     private readonly float movementSpeedMultiplier = 2f;
+    private readonly float decelerationSpeedMultiplier = 3f;
 
-    private float timer;
+    private float currentLegAngle;
+    private float currentArmAngle;
+    private float currentMovementTimer;
+    private float currentSpeedMultiplier;
 
     private void Update()
     {
         if (controller.Speed > 0f)
         {
-            timer += Time.deltaTime * movementSpeedMultiplier * controller.Speed;
+            currentMovementTimer += Time.deltaTime * movementSpeedMultiplier * controller.Speed;
 
-            var legAngle = Mathf.Cos(timer) * legMovementAngle;
-            var armAngle = Mathf.Cos(timer) * armMovementAngle;
-
-            leftLeg.localRotation = Quaternion.Euler(legAngle, leftLeg.localRotation.eulerAngles.y, leftLeg.localRotation.eulerAngles.z);
-            rightLeg.localRotation = Quaternion.Euler(-legAngle, rightLeg.localRotation.eulerAngles.y, rightLeg.localRotation.eulerAngles.z);
-
-            leftArm.localRotation = Quaternion.Euler(leftArm.localRotation.eulerAngles.x, leftArm.localRotation.eulerAngles.y, armRotationOffset - armAngle);
-            rightArm.localRotation = Quaternion.Euler(rightArm.localRotation.eulerAngles.x, rightArm.localRotation.eulerAngles.y, -armRotationOffset - armAngle);
+            if (currentSpeedMultiplier < 1f)
+            {
+                currentSpeedMultiplier += Time.deltaTime * decelerationSpeedMultiplier;
+                currentSpeedMultiplier = Mathf.Min(currentSpeedMultiplier, 1f);
+            }
         }
+        else
+        {
+            if (currentSpeedMultiplier > 0f)
+            {
+                currentSpeedMultiplier -= Time.deltaTime * decelerationSpeedMultiplier;
+                currentSpeedMultiplier = Mathf.Max(currentSpeedMultiplier, 0f);
+            }
+        }
+
+        currentLegAngle = Mathf.Cos(currentMovementTimer) * legMovementAngle * currentSpeedMultiplier;
+        currentArmAngle = Mathf.Cos(currentMovementTimer) * armMovementAngle * currentSpeedMultiplier;
+
+        leftLeg.localRotation = Quaternion.Euler(currentLegAngle, leftLeg.localRotation.eulerAngles.y, leftLeg.localRotation.eulerAngles.z);
+        rightLeg.localRotation = Quaternion.Euler(-currentLegAngle, rightLeg.localRotation.eulerAngles.y, rightLeg.localRotation.eulerAngles.z);
+
+        leftArm.localRotation = Quaternion.Euler(leftArm.localRotation.eulerAngles.x, leftArm.localRotation.eulerAngles.y, armRotationOffset - currentArmAngle);
+        rightArm.localRotation = Quaternion.Euler(rightArm.localRotation.eulerAngles.x, rightArm.localRotation.eulerAngles.y, -armRotationOffset - currentArmAngle);
     }
 }
