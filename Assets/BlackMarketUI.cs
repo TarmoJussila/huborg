4c3fc86b9eb0beea26ac15121208af2e8e0ff4d6
TuomoO <tuomo.oksanen@zaibatsu.fi>
2022-01-29 14:16:20 +0200

    press F to open/close browser

--- Assets/BlackMarketUI.cs
+++ Assets/BlackMarketUI.cs
@@ -1,11 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BlackMarketUI : MonoBehaviour
 {
-    public float maxDistance;
+    public enum UIState
+    {
+        Off,
+        Prompt,
+        Browser
+    }
 
+    public float maxRaycastDistance;
+    public float maxProximityDistance;
+    public UIState currentState = UIState.Off;
     public GameObject browser;
     public GameObject buttonPromptText;
     public GameObject mainCamera;
@@ -23,48 +32,147 @@ public class BlackMarketUI : MonoBehaviour
         {
             return;
         }
-        if (maxDistance > Vector3.Distance(mainCamera.transform.position, transform.position))
+
+        switch (currentState)
+        {
+            case UIState.Off:
+                OffUpdate();
+                break;
+            case UIState.Prompt:
+                PromptUpdate();
+                break;
+            case UIState.Browser:
+                BrowserUpdate();
+                break;
+            default:
+                Debug.Log("Invalid BlackMarketUI.UIState");
+                break;
+        }
+    }
+
+    private void OffUpdate()
+    {
+        float distance = Vector3.Distance(mainCamera.transform.position, transform.position);
+        if (distance <= maxProximityDistance)
+        {
+            setState(UIState.Prompt);
+            return;
+        }
+        else if (distance > maxRaycastDistance)
         {
             return;
         }
 
-
         RaycastHit hit;
-        if (Physics.Raycast(mainCamera.transform.position, transform.forward, out hit, maxDistance))
+        if (Physics.Raycast(mainCamera.transform.position, transform.forward, out hit, maxRaycastDistance))
         {
             if (hit.collider != null)
             {
                 if (hit.collider.gameObject == gameObject)
                 {
-                    //Debug.DrawRay(transform.position, transform.forward, Color.red);
-                    Debug.Log("hit " + Time.time);
-                    setButtonPromt(true);
+
+                    setState(UIState.Prompt);
                 }
             }
         }
     }
 
-    public void setButtonPromt(bool value)
+    private void PromptUpdate()
+    {
+        float distance = Vector3.Distance(mainCamera.transform.position, transform.position);
+        if (distance > maxRaycastDistance)
+        {
+            setState(UIState.Off);
+            return;
+        }
+
+        if (readActionKey())
+        {
+            setState(UIState.Browser);
+        }
+    }
+
+    private void BrowserUpdate()
+    {
+        float distance = Vector3.Distance(mainCamera.transform.position, transform.position);
+        if (distance > maxRaycastDistance)
+        {
+            setState(UIState.Off);
+            return;
+        }
+
+        if (readActionKey())
+        {
+            setState(UIState.Off);
+        }
+    }
+
+    private bool readActionKey()
+    {
+        var keyboard = Keyboard.current;
+        if (keyboard == null)
+        {
+            return false;
+        }
+        return keyboard.fKey.wasPressedThisFrame;
+    }
+
+    private void setButtonPromt(bool value)
     {
         if (!checkAll())
+        {
+            return;
+        }
+        if (buttonPromptText.activeInHierarchy == value)
         {
             return;
         }
         buttonPromptText.SetActive(value);
     }
 
-    public void setBrowser(bool value)
+    private void setBrowser(bool value)
     {
         if (!checkAll())
         {
             return;
         }
-        buttonPromptText.SetActive(!value);
+        if (browser.activeInHierarchy == value)
+        {
+            return;
+        }
         browser.SetActive(value);
     }
 
     private bool checkAll()
     {
         return browser != null && buttonPromptText != null && mainCamera != null;
+    }
+
+    private void setState(UIState newState)
+    {
+        if (currentState == newState)
+        {
+            return;
+        }
+
+        switch(newState)
+        {
+            case UIState.Off:
+                setButtonPromt(false);
+                setBrowser(false);
+                break;
+            case UIState.Prompt:
+                setButtonPromt(true);
+                setBrowser(false);
+                break;
+            case UIState.Browser:
+                setButtonPromt(false);
+                setBrowser(true);
+                break;
+            default:
+                Debug.Log("Invalid BlackMarketUI.UIState");
+                break;
+        }
+        currentState = newState;
     }
 }
