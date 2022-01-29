a3161faa3c55cc484d852829cfc5429534963c87
Tarmo Jussila <tarmo.jussila@zaibatsu.fi>
2022-01-29 17:35:49 +0200

    WIP: Character accessories.

---
+++ Assets/Scripts/CharacterAccessoryHandler.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class CharacterAccessoryHandler : MonoBehaviour
+{
+    [SerializeField] private Transform leftLeg;
+    [SerializeField] private Transform rightLeg;
+
+    [SerializeField] private Transform leftArm;
+    [SerializeField] private Transform rightArm;
+}
