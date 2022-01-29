dc2590a80d7b4c282b6128895edeb2d28419971c
Kalle <kalle.collan@zaibatsu.fi>
2022-01-30 00:11:15 +0200

    Basic inventory. Player can pickup items by pressing F. Swap items by pressing E. Throw items by pressing G.

---
+++ Assets/Scripts/PickableObject.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickableObject : MonoBehaviour
+{
+    public enum ObjectType
+    {
+        Default,
+        Pasta,
+        Jauheliha,
+        Lonkero,
+        Money,
+        EmptyHand
+    }
+
+    public ObjectType Type = ObjectType.Default;
+    public bool IsPicked = false;
+}
