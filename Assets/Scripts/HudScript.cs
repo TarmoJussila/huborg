f310ae3035c4a2106dfdabd89d439473e3b26536
Tarmo Jussila <tarmo.jussila@zaibatsu.fi>
2022-01-30 12:02:34 +0200

    Cleaned some unused code.

--- Assets/Scripts/HudScript.cs
+++ Assets/Scripts/HudScript.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class HudScript : MonoBehaviour
@@ -11,18 +9,6 @@ public class HudScript : MonoBehaviour
 
     public int currentMoney = 0;
     public TMPro.TextMeshProUGUI hudMoneyText;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     /// <summary>
     /// Add money
